bf7b924d6123de7f3551564be29cbd0c80d0c784
毛 <1296193245@qq.com>
2022-04-06 21:40:48 +0800

    Mybatis_Plus_conditional_constructor_fuzzy_query

---
+++ src/test/java/mapper/StudentMapperTest.java
@@ -0,0 +1,143 @@
+package mapper;
+
+import com.baomidou.mybatisplus.core.MybatisSqlSessionFactoryBuilder;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import data.Student;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Project name(项目名称)：Mybatis_Plus条件构造器之模糊查询
+ * Package(包名): mapper
+ * Class(测试类名): StudentMapperTest
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/4/6
+ * Time(创建时间)： 21:30
+ * Version(版本): 1.0
+ * Description(描述)： 测试类
+ * like
+ * LIKE '%值%'
+ * 例: like("name", "王") ---> name like '%王%'
+ * notLike
+ * NOT LIKE '%值%
+ * 例: notLike("name", "王") ---> name not like '%王%'
+ * likeLeft
+ * LIKE '%值'
+ * 例: likeLeft("name", "王") ---> name like '%王'
+ * likeRight
+ * LIKE '值%'
+ * 例: likeRight("name", "王") ---> name like '王%'
+ */
+
+class StudentMapperTest
+{
+    @Test
+    public void like() throws IOException
+    {
+        String resource = "mybatis-config.xml";
+        //读取配置文件mybatis-config.xml
+        InputStream config = Resources.getResourceAsStream(resource);
+        //根据配置文件构建SqlSessionFactory
+        SqlSessionFactory sqlSessionFactory = new MybatisSqlSessionFactoryBuilder().build(config);
+        //通过SqlSessionFactory创建SqlSession
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        StudentMapper studentMapper = sqlSession.getMapper(StudentMapper.class);
+
+        QueryWrapper<Student> queryWrapper = new QueryWrapper<>();
+        queryWrapper.like("name", "李");
+
+        List<Student> list = studentMapper.selectList(queryWrapper);
+        for (Student student : list)
+        {
+            System.out.print(student);
+        }
+        System.out.println("数量：" + list.size());
+
+        sqlSession.close();
+    }
+
+    @Test
+    public void notLike() throws IOException
+    {
+        String resource = "mybatis-config.xml";
+        //读取配置文件mybatis-config.xml
+        InputStream config = Resources.getResourceAsStream(resource);
+        //根据配置文件构建SqlSessionFactory
+        SqlSessionFactory sqlSessionFactory = new MybatisSqlSessionFactoryBuilder().build(config);
+        //通过SqlSessionFactory创建SqlSession
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        StudentMapper studentMapper = sqlSession.getMapper(StudentMapper.class);
+
+        QueryWrapper<Student> queryWrapper = new QueryWrapper<>();
+        queryWrapper.notLike("name", "李");
+
+        List<Student> list = studentMapper.selectList(queryWrapper);
+        for (Student student : list)
+        {
+            System.out.print(student);
+        }
+        System.out.println("数量：" + list.size());
+
+        sqlSession.close();
+    }
+
+    @Test
+    public void likeLeft() throws IOException
+    {
+        String resource = "mybatis-config.xml";
+        //读取配置文件mybatis-config.xml
+        InputStream config = Resources.getResourceAsStream(resource);
+        //根据配置文件构建SqlSessionFactory
+        SqlSessionFactory sqlSessionFactory = new MybatisSqlSessionFactoryBuilder().build(config);
+        //通过SqlSessionFactory创建SqlSession
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        StudentMapper studentMapper = sqlSession.getMapper(StudentMapper.class);
+
+        QueryWrapper<Student> queryWrapper = new QueryWrapper<>();
+        queryWrapper.likeLeft("name", "光");
+
+        List<Student> list = studentMapper.selectList(queryWrapper);
+        for (Student student : list)
+        {
+            System.out.print(student);
+        }
+        System.out.println("数量：" + list.size());
+
+        sqlSession.close();
+    }
+
+    @Test
+    public void likeRight() throws IOException
+    {
+        String resource = "mybatis-config.xml";
+        //读取配置文件mybatis-config.xml
+        InputStream config = Resources.getResourceAsStream(resource);
+        //根据配置文件构建SqlSessionFactory
+        SqlSessionFactory sqlSessionFactory = new MybatisSqlSessionFactoryBuilder().build(config);
+        //通过SqlSessionFactory创建SqlSession
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        StudentMapper studentMapper = sqlSession.getMapper(StudentMapper.class);
+
+        QueryWrapper<Student> queryWrapper = new QueryWrapper<>();
+        queryWrapper.likeRight("name", "陈");
+
+        List<Student> list = studentMapper.selectList(queryWrapper);
+        for (Student student : list)
+        {
+            System.out.print(student);
+        }
+        System.out.println("数量：" + list.size());
+
+        sqlSession.close();
+    }
+}
